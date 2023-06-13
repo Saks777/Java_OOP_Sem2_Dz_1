61f21b70d626b250ed9400ca90132955c491f594
Saks777 <sb.sakenyunusov@gmail.com>
2023-06-13 22:29:00 +0500

    DZ2

---
+++ BasicCalc.java
@@ -0,0 +1,13 @@
+package Seminar_2_Dz_1;
+
+public interface BasicCalc {
+    void add(double value); //сложение
+
+    void subtract(double value); //вычитание
+
+    void multiply(double value); //умножение
+
+    void divide(double value); //деление
+
+    double getResult();
+}
